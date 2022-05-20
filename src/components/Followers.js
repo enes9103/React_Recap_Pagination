ddb7b99004c2b3d09a038adfa372b7bb550ae977
EnesUnlu <enes9103@gmail.com>
2022-05-20 21:56:39 +0300

    finally

--- src/components/Followers.js
+++ src/components/Followers.js
@@ -1,42 +1,62 @@
-import React from "react";
 import axios from "axios";
-import { useEffect } from "react";
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Row, Col, Card, Container, Button } from "react-bootstrap";
-
+import Paginate from "./Paginate";
 const Followers = () => {
   const [followersList, setFollowersList] = useState([]);
   const [loading, setLoading] = useState(true);
-
+  const [currentPage, setCurrentPage] = useState(1)
+  const [followersPerPage] = useState(10)
+  const indexOfLastFollowers = currentPage * followersPerPage
+  const indexOfFirstFollowers = indexOfLastFollowers - followersPerPage
+  const currentFollower = followersList.slice(indexOfFirstFollowers, indexOfLastFollowers)
+  const totalPagesNum = Math.ceil(followersList.length / followersPerPage)
   const getFollowers = async () => {
-    const {data} = await axios.get("https://api.github.com/users/enes9103/followers?per_page=100");
-    
-      setFollowersList(data)
-      console.log(data);
+    const { data } = await axios.get(
+      "https://api.github.com/users/zeyasar/followers?per_page=100"
+    );
+    setFollowersList(data);
+    console.log(data);
   };
-
   useEffect(() => {
     getFollowers();
   }, []);
-
   return (
-    <div>
+    <>
       <Container>
         <Row xs={2} md={3} lg={4} className="g-4 mt-4">
-          {followersList.map((follower, index) => (
+          {currentFollower.map((follower, index) => (
             <Col key={index}>
               <Card>
                 <Card.Img variant="top" src={follower.avatar_url} />
                 <Card.Body>
                   <Card.Text>{follower.login}</Card.Text>
-                  <Button href={follower.html_url} style={{borderRadius:"50px"}} variant="primary">VIEW PROFILE</Button>
+                  <Button
+                    href={follower.html_url}
+                    style={{ borderRadius: "50px" }}
+                    variant="primary"
+                  >
+                    VIEW PROFILE
+                  </Button>
                 </Card.Body>
               </Card>
             </Col>
           ))}
         </Row>
       </Container>
-    </div>
+      <Container className="d-flex justify-content-center mt-4">
+       <Paginate pages= {totalPagesNum} setCurrentPage={setCurrentPage}/>
+      </Container>
+    </>
   );
 };
 export default Followers;
+
+
+
+
+
+
+
+
+
--- src/components/Paginate.js
+++ src/components/Paginate.js
@@ -1,11 +1,34 @@
-import React from 'react'
-
-const Paginate = () => {
+import React, { useEffect, useState } from 'react'
+import {Pagination} from 'react-bootstrap'
+const Paginate = ({pages, setCurrentPage}) => {
+    
+    const [currentButton, setCurrentButton] = useState(1)
+    const numOfPages = []
+    for (let i = 1; i <= pages; i++) {
+        numOfPages.push(i)
+        
+    }
+     useEffect(() =>{
+         setCurrentPage(currentButton)
+     }, [currentButton])
   return (
     <div>
-      
+         <Pagination>
+         
+         <Pagination.Prev className={`${ currentButton === 1 ? 'disabled' : null}`} onClick={()=>setCurrentButton((prev)=>prev === 1 ? prev : prev -1)}/>
+         
+         {
+             numOfPages.map((page,index)=>{
+                 return(
+                    <Pagination.Item className={`${currentButton === page ? 'active' : null}`} onClick={() => setCurrentButton(page)} key={index}>{page}</Pagination.Item>
+                 )
+             })
+            
+         }
+         <Pagination.Next className={`${numOfPages.length === currentButton ? 'disabled' : null}`} onClick={()=>setCurrentButton((prev)=>prev === numOfPages.length ? prev : prev +1)}/>
+       
+       </Pagination>
     </div>
   )
 }
-
 export default Paginate
